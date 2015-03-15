b47289dc2a972d8edd52f3c21ade7ffe99eb5a9b
klimentlambevski <kliment.lambevski@gmail.com>
2015-03-15 16:20:59 +0100

    init with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularUploaderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularUploaderApp
+ */
+angular.module('angularUploaderApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularUploaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularUploaderApp
+ */
+angular.module('angularUploaderApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
